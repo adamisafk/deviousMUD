3f87c0933c09776a115b3857025a40c8db998133
Adam Howells <adam.howells@dvla.gov.uk>
2020-12-07 11:54:32 +0000

    Add Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main
+{
+    static Scanner stdin = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        System.out.print("Test");
+    }
+}
