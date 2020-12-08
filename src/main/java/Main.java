b959c48017083326a9b36798c29021c030af50ba
Harley Burrow <he.burrow@yahoo.co.uk>
2020-12-08 11:06:51 +0000

    Added Ascii art in Game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import org.example.Game;
+
 import java.util.*;
 public class Main
 {
     static Scanner stdin = new Scanner(System.in);
     public static void main(String[] args)
     {
-        System.out.print("Test");
+
+        Game game = new Game();
+        game.asciiArt();
     }
+
 }
--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -11,5 +11,47 @@ public class Game {
 
     public Game() {
     }
+
+
+    public void asciiArt(){
+
+        System.out.println("88888888ba,                             88                                          88b           d88  88        88  88888888ba,                            _,.-------.,_                      ");
+        System.out.println("88      `\"8b                            \"\"                                          888b         d888  88        88  88      `\"8b                       ,;~'             '~;,                      ");
+        System.out.println("88        `8b                                                                       88`8b       d8'88  88        88  88        `8b                    ,;                     ;,                                         ");
+        System.out.println("88         88   ,adPPYba,  8b       d8  88   ,adPPYba,   88       88  ,adPPYba,     88 `8b     d8' 88  88        88  88         88                   ;                         ;                                             ");
+        System.out.println("88         88  a8P_____88  `8b     d8'  88  a8\"     \"8a  88       88  I8[    \"\"     88  `8b   d8'  88  88        88  88         88                  ,'                         ',                                            ");
+        System.out.println("88         8P  8PP\"\"\"\"\"\"\"   `8b   d8'   88  8b       d8  88       88   `\"Y8ba,      88   `8b d8'   88  88        88  88         8P                 ,;                           ;,                                            ");
+        System.out.println("88      .a8P   \"8b,   ,aa    `8b,d8'    88  \"8a,   ,a8\"  \"8a,   ,a88  aa    ]8I     88    `888'    88  Y8a.    .a8P  88      .a8P                  ; ;      .           .      ; ;                                         ");
+        System.out.println("88888888Y\"'     `\"Ybbd8\"'      \"8\"      88   `\"YbbdP\"'    `\"YbbdP'Y8  `\"YbbdP\"'     88     `8'     88   `\"Y8888Y\"'   88888888Y\"'                   | ;   ______       ______   ; |                                             ");
+        System.out.println("                                                                                                                                                   |  `/~\\\"     ~\" . \"~     \"~\\' |                                                                          ");
+        System.out.println("                                                                                                                                                   |  ~  ,-~~~^~, | ,~^~~~-,  ~  |                                                                            ");
+        System.out.println("                                                                                                                                                    |   |        }:{        |   |                                                                       ");
+        System.out.println("                                                                                                                                                    |   l       / | \\       !   |                                                                       ");
+        System.out.println("                                                                                                                                                     .~  (__,.--\" .^. \"--.,__)  ~.                                                                   ");
+        System.out.println("                                                                                                                                                    |     ---;' / | \\ `;---     |                                                                  ");
+        System.out.println("                                                                                                                                                    \\__.      \\/^\\/ \\/^\\     .__/                                                                      ");
+        System.out.println("                                                                                                                                                                                                                          ");
+        System.out.println("                                                                                                                                                                                                                              ");
+        System.out.println("                                                                                                                                                                                                                              ");
+        System.out.println("                                                                                                                                                                                                                              ");
+        System.out.println("                                                                                                                                                                                                                              ");
+        System.out.println("                                                                                                                                                                                                                              ");
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
 }
+
+
+
 
