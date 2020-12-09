fd148455e0fcb80d36ac70431172ddc7c066c5f6
Finchley Harwood <finchley.harwood@testmail.com>
2020-12-09 11:03:47 +0000

    Fixed issue in getEntityManagerFactory, to do with if(factory == null).

--- src/main/java/org/example/entity/JPAUtil.java
+++ src/main/java/org/example/entity/JPAUtil.java
@@ -12,9 +12,9 @@ public class JPAUtil {
     private static EntityManagerFactory factory;
 
     public static EntityManagerFactory getEntityManagerFactory() {
-        if (factory == null) {
-            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-        }
+
+        factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+
         return factory;
     }
     public static void shutdown() {
