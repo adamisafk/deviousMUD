6e15713fdff7c0c3e3efbd912a93c8dc7fafa8a5
Adam Howells <adam.howells@dvla.gov.uk>
2020-12-09 15:14:19 +0000

    Add a loop of newlines to 'clear' the hibernate log on app start

--- src/main/java/org/example/entity/JPAUtil.java
+++ src/main/java/org/example/entity/JPAUtil.java
@@ -14,6 +14,7 @@ public class JPAUtil {
     public static EntityManagerFactory getEntityManagerFactory() {
         if(factory == null) {
             factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+            for (int i = 0; i < 50; ++i) System.out.println();
         }
         return factory;
     }
