3d982b5dc582c79c8a5ba5f986ac029fa8de763e
Adam Howells <adam.howells@dvla.gov.uk>
2020-12-10 14:05:53 +0000

    Create unit test for getItemByNPCId

--- src/test/java/org/example/entity/JPAUtilTest.java
+++ src/test/java/org/example/entity/JPAUtilTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Tests to check that the JPAUtil class is working as expected.
@@ -101,6 +100,15 @@ public class JPAUtilTest {
         assertEquals(3, JPAUtil.getNoOfEntries("Chest"));
     }
 
-
-
+    // Test the functions involving relationships
+    @Test
+    void testGetItemByNPCId() throws Exception {
+        assertEquals(6, JPAUtil.getItemByNPCId(1).getId());
+        assertEquals("Fire Bolt Spell", JPAUtil.getItemByNPCId(1).getName());
+        assertEquals(5, JPAUtil.getItemByNPCId(1).getMagicDamage());
+        assertEquals(10, JPAUtil.getItemByNPCId(2).getId());
+        assertEquals(3, JPAUtil.getItemByNPCId(3).getId());
+        assertEquals(3, JPAUtil.getItemByNPCId(4).getId());
+        assertEquals(7, JPAUtil.getItemByNPCId(5).getId());
+    }
 }
